a940233c45eea6cb011b3b2658a73f0943ee2665
Kris-Mikael Krister <krismikael@protonmail.ch>
2016-06-13 06:30:11 +0200

    Updates, add jsx section

--- code/1_babel/app.js
+++ code/1_babel/app.js
@@ -1,88 +1,82 @@
-class Header extends React.Component {
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    render() {
-        console.log('render header');
-        return <div>Hello {this.props.name}</div>;
-    }
-}
-
-class EmployeeList extends React.Component {
-    render() {
-        console.log('render employeelist');
-        const {employees} = this.props;
-        return (
-            <ul>
-                {employees.map(employee => <li key={employee.id}>{employee.name}</li>)}
-            </ul>
-        );
-    }
-}
-
-class AddEmployeeForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 'Foo'
-        };
-    }
-
-    onChange(e) {
-        this.setState({
-            value: e.target.value
-        });
-    }
-
-    onClick() {
-        this.props.onAdd(this.state.value);
-    }
-
-    render() {
-        console.log('render AddEmployeeForm');
-        return (
-            <div>
-                <input
-                    value={this.state.value}
-                    onChange={this.onChange.bind(this)}/>
-                <button onClick={this.onClick.bind(this)}>Legg til</button>
-            </div>
-        );
-    }
-}
-
 class App extends React.Component {
     constructor() {
         super();
-
         this.state = {
             employees: [
-                {id: 1, name: 'Terje'},
-                {id: 2, name: 'Kris-Mikael'}
+                {id: 1, name: "Terje"},
+                {id: 2, name: "Ole"}
             ]
         };
     }
-    onAdd(employeeName) {
-        const newId = this.state.employees.length + 1;
+
+    employeeAdded(employeeName) {
+        const nextId = this.state.employees.length + 1;
         this.setState({
             employees: [...this.state.employees, {
-                id: newId,
+                id: nextId,
                 name: employeeName
             }]
         });
     }
 
     render() {
-        console.log('render App');
         return (
             <div>
-                <Header name="Nøff"/>
+                <Header name="Kris-Mikael"/>
                 <EmployeeList employees={this.state.employees}/>
-                <AddEmployeeForm onAdd={this.onAdd.bind(this)}/>
+                <AddEmployeeForm onAddEmployee={this.employeeAdded.bind(this)}/>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('my-app'));
+class Header extends React.Component {
+    render() {
+        return <div>Hei {this.props.name}</div>;
+    }
+}
+
+class EmployeeList extends React.Component {
+    render() {
+        const {employees} = this.props;
+        return (
+            <ul>
+                {employees.map(employee => (
+                    <li key={employee.id}>{employee.name}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+class AddEmployeeForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            inputValue: ""
+        };
+    }
+
+    valueChanged(e) {
+        this.setState({
+            inputValue: e.target.value
+        });
+    }
+
+    buttonClicked() {
+        this.props.onAddEmployee(this.state.inputValue);
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.inputValue}
+                    onChange={this.valueChanged.bind(this)}/>
+                <button onClick={this.buttonClicked.bind(this)}>Legg til</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById("my-app"));
